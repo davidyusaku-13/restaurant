b6ccf2dde5e5dbeb7b49e343bf088bfe31f71342
David Yusaku <davidyusaku13@gmail.com>
2023-01-27 00:56:37 +0700

    lots of updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { MdOutlineRestaurantMenu } from "react-icons/md";
-import images from "../../constants/images";
-import "./Navbar.css";
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images';
+import './Navbar.css';
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.gericht} alt="app logo" />
+        <img src={images.gericht} alt="app__logo" />
       </div>
       <ul className="app__navbar-links">
         <li className="p__opensans"><a href="#home">Home</a></li>
@@ -22,12 +22,13 @@ const Navbar = () => {
         <a href="#login" className="p__opensans">Log In / Register</a>
         <div />
         <a href="/" className="p__opensans">Book Table</a>
+      </div>
         <div className="app__navbar-smallscreen">
-          <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)}/>
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
           {toggleMenu && (
             <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
               <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)}/>
-              <ul className="app__navbar-smallscreen-links">
+              <ul className="app__navbar-smallscreen_links">
                 <li className="p__opensans"><a href="#home">Home</a></li>
                 <li className="p__opensans"><a href="#about">About</a></li>
                 <li className="p__opensans"><a href="#menu">Menu</a></li>
@@ -37,7 +38,6 @@ const Navbar = () => {
             </div>
           )}
         </div>
-      </div>
     </nav>
   );
 };
