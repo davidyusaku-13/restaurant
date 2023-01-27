6200d0539d88a285f1e1e9a73cc2e130825e77e7
David Yusaku <davidyusaku13@gmail.com>
2023-01-28 01:16:20 +0700

    small changes

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { FooterOverlay, Newsletter } from '../../components';
+import React from "react";
+import { FooterOverlay, Newsletter } from "../../components";
 
-import {FiFacebook,FiTwitter,FiInstagram} from 'react-icons/fi'
-import './Footer.css';
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import "./Footer.css";
 
 const Footer = () => (
-  <div className='app__footer section__padding'>
-    <FooterOverlay/>
-    <Newsletter/>
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { SubHeading } from '../../components';
+import React from "react";
+import { SubHeading } from "../../components";
 
-import {images} from '../../constants';
-import './Header.css';
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div className='app__header app__wrapper section__padding' id='home'>
-    <div className='app__wrapper_info'>
-      <SubHeading title='Chase the new flavour'/>
-      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
-      <p className='p__opensans' style={{margin:'2rem 0'}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta a doloremque error corporis possimus repellat fugiat ratione, consequatur illum consequuntur deleniti ipsum molestias dicta? A distinctio eum voluptas nihil hic?</p>
-      <button type='button' className='custom__button'>Explore Menu</button>
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta a
+        doloremque error corporis possimus repellat fugiat ratione, consequatur
+        illum consequuntur deleniti ipsum molestias dicta? A distinctio eum
+        voluptas nihil hic?
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
 
-    <div className='app__wrapper_img'>
+    <div className="app__wrapper_img">
       <img src={images.welcome} alt="header__img" />
     </div>
   </div>
